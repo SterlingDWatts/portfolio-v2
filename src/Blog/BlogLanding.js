65fa5d0cdfb245916d9057638e1f01763ee2c92f
Sterling Watts <SterlingDWatts@gmail.com>
2020-02-02 17:36:48 -0800

    remove flex-wrap

---
+++ src/Blog/BlogLanding.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./BlogLanding.css";
+
+class BlogLanding extends Component {
+  render() {
+    return <main className="BlogLanding"></main>;
+  }
+}
+
+export default BlogLanding;
